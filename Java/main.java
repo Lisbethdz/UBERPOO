29c40becf744f7824f904f64ca4a449d4a2c17b3
Jeymy Martir <lisbethmartir511@gmail.com>
2024-04-05 13:45:14 -0600

    Agregando Archivo

---
+++ Java/main.java
@@ -0,0 +1,20 @@
+class main {
+    public static void main (String)[] args){
+        System.out.println 
+        ("Inicializando"...);
+        System.out.println ("Car..");
+        Car car = new Car ("AMQ123", new
+        Account("Andres Loiza", "ADN12435" , "andres@hotmail.com",
+        ar.passenger = 4;
+
+car.printDatacar();
+
+System.out.println("Uberx....");
+
+Uberx uberx = new UberX
+
+("MKL185", new Account ("Maria Loyola", "JKL12365", "marial@hotmail.com", "125478"), "Toyota", "Corolla");
+
+    } 
+
+}
